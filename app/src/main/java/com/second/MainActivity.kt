a7ee6329367e796a5fd0fbc68ab4a11a93038163
Sunny <sl2883@cornell.edu>
2021-05-04 16:28:47 -0700

    adding product config

--- app/src/main/java/com/second/MainActivity.kt
+++ app/src/main/java/com/second/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.second
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.clevertap.android.sdk.CleverTapAPI
+import com.clevertap.android.sdk.Logger
+import com.clevertap.android.sdk.product_config.CTProductConfigListener
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var clevertapi = CleverTapAPI.getDefaultInstance(getApplicationContext());
+        CleverTapAPI.createNotificationChannel(getApplicationContext(), "generic", "generic", "Game Of Thrones", NotificationManager.IMPORTANCE_MAX, true, "myring.mp3");
+
+        CleverTapAPI.setDebugLevel(3);
+
+        val cleverTapAPI = CleverTapAPI.getDefaultInstance(applicationContext)
+
+        cleverTapAPI?.productConfig()?.setDefaults(R.xml.product_config_defaults)
+        cleverTapAPI?.productConfig()?.fetchAndActivate();
+
+        cleverTapAPI!!.setCTProductConfigListener(object : CTProductConfigListener {
+            override fun onInit() {
+                //Must Call activate if you want to apply the last fetched values on init every time.
+                cleverTapAPI!!.productConfig().activate()
+            }
+
+            override fun onFetched() {
+                var pc = cleverTapAPI?.productConfig();
+                Logger.d("Clevertap Config");
+                Logger.d(pc?.getString("reward type"));
+                Logger.d("Clevertap Config");
+                Logger.d(pc?.getString("price"));
+                Logger.d("Clevertap Config");
+            }
+            override fun onActivated() {}
+        })
+
     }
 }
